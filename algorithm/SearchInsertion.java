22e9731e5bf31049356efbe345b3c2f97c9809af
mrsam468 <ibenjaminsamuel47@gmail.com>
2025-11-02 22:21:22 +0100

    SearchInsertion

---
+++ algorithm/SearchInsertion.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+public class SearchInsertion {
+    public int searchInsert(int[] nums,int target){
+        int j =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                j=i;
+            } else if (nums[i]<target) {
+                j=i+1;
+            }
+        }
+        return j;
+    }
+    public static void main(String[] args){
+        SearchInsertion search = new SearchInsertion();
+        System.out.println(search.searchInsert(new int[]{1, 4, 5,6,7},4));
+    }
+}
